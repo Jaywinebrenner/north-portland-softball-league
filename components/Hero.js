4a649a5a2574729be332c5d26aff00446f782bf2
Jay WInebrenner <jaywinebrenner@gmail.com>
2022-08-17 17:35:09 -0700

    first commit

---
+++ components/Hero.js
@@ -0,0 +1,19 @@
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="hero__wrapper">
+                <img src="/mascots.jpg"/>
+                <div className="right">
+                    <h1 className="hero-main-text">Welcome to the North Portland Softball League</h1>
+                    <p>Established 2008</p>
+                    <div class="swirl">
+                        <img src="/swirl.gif" alt="" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+export default Hero
---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h3>Schedule</h3>
+            <h3>About</h3>
+            <h3>Current Teams</h3>
+            <h3>Past Winners</h3>
+        </div>
+    )
+}
+
+export default Navbar
