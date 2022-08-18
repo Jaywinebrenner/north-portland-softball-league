52886bce54a8be51fd77de97441b393b3e9440cd
Jay WInebrenner <jaywinebrenner@gmail.com>
2022-08-18 13:26:38 -0700

    fix deploy error

--- components/Hero.js
+++ components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
                 <div className="right">
                     <h1 className="hero-main-text">Welcome to the North Portland Softball League</h1>
                     <p>Established 2008</p>
-                    <div class="swirl">
+                    <div className="swirl">
                         <img src="/swirl.gif" alt="" />
                     </div>
                 </div>
